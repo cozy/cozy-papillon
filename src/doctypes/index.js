bf2e01587d79877c47112d2d6801988b9dc77e69
Vince Linise <vincelinise@gmail.com>
2024-08-09 13:34:16 +0200

    feat(queries): Added timetable query and doctype

--- src/doctypes/index.js
+++ src/doctypes/index.js
@@ -1,5 +1,8 @@
 // the documents schema, necessary for CozyClient
 export default {
+  timetable: {
+    doctype: 'io.cozy.calendar.event'
+  },
   homeworks: {
     doctype: 'io.cozy.calendar.todos'
   },
