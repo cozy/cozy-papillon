8f55d5ee803a000d5a3dfa492989f83a17a8ec13
Vince Linise <vincelinise@gmail.com>
2024-08-14 10:50:52 +0200

    style(presence): Removed unused imports

--- src/components/Views/PresenceView.jsx
+++ src/components/Views/PresenceView.jsx
@@ -2,25 +2,16 @@ import React from 'react'
 import { buildPresenceQuery } from 'src/queries'
 
 import { useQuery } from 'cozy-client'
-import Chip from 'cozy-ui/transpiled/react/Chips'
-import Divider from 'cozy-ui/transpiled/react/Divider'
 import Empty from 'cozy-ui/transpiled/react/Empty'
-import Icon from 'cozy-ui/transpiled/react/Icon'
-import BellIcon from 'cozy-ui/transpiled/react/Icons/Bell'
-import ClockIcon from 'cozy-ui/transpiled/react/Icons/Clock'
 import CozyIcon from 'cozy-ui/transpiled/react/Icons/Cozy'
-import WalkIcon from 'cozy-ui/transpiled/react/Icons/Walk'
 import List from 'cozy-ui/transpiled/react/List'
-import ListItem from 'cozy-ui/transpiled/react/ListItem'
-import ListItemIcon from 'cozy-ui/transpiled/react/ListItemIcon'
-import ListItemText from 'cozy-ui/transpiled/react/ListItemText'
 import ListSubheader from 'cozy-ui/transpiled/react/ListSubheader'
 import Typography from 'cozy-ui/transpiled/react/Typography'
 import useBreakpoints from 'cozy-ui/transpiled/react/providers/Breakpoints'
 import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
-import { TabTitle } from '../Atoms/TabTitle'
 import { PresenceItem } from '../Atoms/Presence/PresenceItem'
+import { TabTitle } from '../Atoms/TabTitle'
 
 export const PresenceView = () => {
   const { t } = useI18n()
