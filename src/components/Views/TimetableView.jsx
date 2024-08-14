e0bf0f427c3ad2ca533c20cc0946580d4bb03063
Vince Linise <vincelinise@gmail.com>
2024-08-14 10:41:13 +0200

    refactor(timetable): Created a distinct individual day component

--- src/components/Views/TimetableView.jsx
+++ src/components/Views/TimetableView.jsx
@@ -3,8 +3,6 @@ import { Outlet } from 'react-router-dom'
 import { buildTimetableQuery } from 'src/queries'
 
 import { useQuery } from 'cozy-client'
-import List from 'cozy-ui/transpiled/react/List'
-import ListSubheader from 'cozy-ui/transpiled/react/ListSubheader'
 import { CircularProgress } from 'cozy-ui/transpiled/react/Progress'
 import Typography from 'cozy-ui/transpiled/react/Typography'
 import useBreakpoints from 'cozy-ui/transpiled/react/providers/Breakpoints'
@@ -12,7 +10,7 @@ import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
 import { TabTitle } from '../Atoms/TabTitle'
 import { CozyDatePickerInline } from '../Atoms/Timetable/CozyDatePickerInline'
-import { TimetableItem } from '../Atoms/Timetable/TimetableItem'
+import { TimetableDay } from '../Atoms/Timetable/TimetableDay'
 
 export const TimetableView = () => {
   const { t } = useI18n()
@@ -99,42 +97,11 @@ export const TimetableView = () => {
               }
 
               return (
-                <div
+                <TimetableDay
+                  day={day}
+                  timetable={timetable}
                   key={day.toISOString()}
-                  style={{
-                    overflowX: 'hidden'
-                  }}
-                  className="u-flex u-flex-column u-w-100 u-h-100"
-                >
-                  <List>
-                    <ListSubheader>
-                      {day.toLocaleDateString('default', {
-                        weekday: 'long',
-                        day: '2-digit'
-                      })}
-                    </ListSubheader>
-
-                    {(
-                      timetable.find(
-                        group => group.date === day.toISOString()
-                      ) ?? { courses: [] }
-                    )?.courses.map(course => (
-                      <TimetableItem course={course} key={course._id} />
-                    ))}
-
-                    {(
-                      timetable.find(
-                        group => group.date === day.toISOString()
-                      ) ?? { courses: [] }
-                    )?.courses.length === 0 && (
-                      <div className="u-flex u-flex-column u-flex-justify-center u-flex-items-center u-p-1 u-w-100">
-                        <Typography variant="body2" color="textSecondary">
-                          {t('Timetable.noCourses')}
-                        </Typography>
-                      </div>
-                    )}
-                  </List>
-                </div>
+                />
               )
             })}
           </div>
