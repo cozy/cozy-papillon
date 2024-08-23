f81e1bd15f6aacf9714eeb6e23c61080f245ef67
Vince Linise <vincelinise@gmail.com>
2024-08-23 13:16:48 +0200

    publish: Merge pull request #26 from cozy/timetables/memoization
    generated from commit 8300482baf030b55b845be6beaea90da09ad6950

--- service-worker.js
+++
@@ -1,1 +0,0 @@
-// This file is needed for an correct installation of SW, which is required to allow the user to refresh their PWA
--- vendors.358fe675cff021e18814.hot-update.js
+++
@@ -1,6 +0,0 @@
-webpackHotUpdate("vendors",{
-
-/***/ "./node_modules/cozy-ui/transpiled/react/Icons/Left.js":
-false
-
-})
