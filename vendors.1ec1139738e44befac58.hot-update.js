e603facfddbf7b03925ae14494726cc56186d524
Vince Linise <vincelinise@gmail.com>
2024-08-31 23:28:32 +0200

    publish: feat: Added student identity to account switcher
    generated from commit 4834ad9b00b487d98af589c3f021f107df671dfa

---
+++ vendors.1ec1139738e44befac58.hot-update.js
@@ -0,0 +1,12 @@
+webpackHotUpdate("vendors",{
+
+/***/ "./node_modules/chart.js/dist/chart.esm.js":
+false,
+
+/***/ "./node_modules/chart.js/dist/chunks/helpers.segment.js":
+false,
+
+/***/ "./node_modules/react-chartjs-2/dist/index.js":
+false
+
+})
